3ebb9d992057d23e0d2ff24d1a6a1a640261f261
zhangliang0115 <zhangliang0115@125.com>
2019-04-30 14:10:19 +0800

    brand01

--- src/test/java/com/example/git/GitBranchDemoApplicationTests.java
+++ src/test/java/com/example/git/GitBranchDemoApplicationTests.java
@@ -5,9 +5,14 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.AbstractCollection;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class GitBranchDemoApplicationTests {
+
+
+	public void get(){}
 
 	@Test
 	public void contextLoads() {
