f065549dc88987dd4eafa41de682bd61340e1aec
zhangliang0115 <zhangliang0115@125.com>
2019-04-30 13:50:21 +0800

    brand01

--- src/test/java/com/example/git/GitBranchDemoApplicationTests.java
+++ src/test/java/com/example/git/GitBranchDemoApplicationTests.java
@@ -11,6 +11,11 @@ public class GitBranchDemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
+
+	}
+
+	public static void main(String[] args) {
+		System.out.println("test demo");
 	}
 
 }
