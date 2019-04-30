f669e2d6d4d73175629175921fb5eb2441084a92
zhangliang0115 <zhangliang0115@125.com>
2019-04-30 14:14:51 +0800

    brand01

---
+++ src/test/java/com/example/git/MasterController.java
@@ -0,0 +1,11 @@
+package com.example.git;
+
+/**
+ * @ClassName MasterController
+ * @Description TODO
+ * @Author zhangliang
+ * @Date 2019/4/30 14:14
+ * @Version 1.0
+ **/
+public class MasterController {
+}
