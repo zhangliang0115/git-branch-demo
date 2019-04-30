9c8ba4ee66154061ea2ec56f23dd8f7cd9a4b918
zhangliang0115 <zhangliang0115@125.com>
2019-04-30 14:53:01 +0800

    brand01

---
+++ src/test/java/com/example/git/News4Controller.java
@@ -0,0 +1,11 @@
+package com.example.git;
+
+/**
+ * @ClassName NewsController
+ * @Description TODO
+ * @Author zhangliang
+ * @Date 2019/4/30 14:13
+ * @Version 1.0
+ **/
+public class News4Controller {
+}
