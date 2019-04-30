a7cd37ef039029ed7ba0f1a42cac41ac4a77a39a
zhangliang0115 <zhangliang0115@125.com>
2019-04-30 13:44:48 +0800

    Initial commit

---
+++ src/main/java/com/example/git/GitBranchDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitBranchDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitBranchDemoApplication.class, args);
+	}
+
+}
