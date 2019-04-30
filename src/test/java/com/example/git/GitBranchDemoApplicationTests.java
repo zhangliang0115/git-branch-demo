95aaaa3fb8e4e645787a47f74beaa7a154f150c2
zhangliang0115 <zhangliang0115@125.com>
2019-04-30 15:00:42 +0800

    brand01

--- src/test/java/com/example/git/GitBranchDemoApplicationTests.java
+++ src/test/java/com/example/git/GitBranchDemoApplicationTests.java
@@ -20,7 +20,7 @@ public class GitBranchDemoApplicationTests {
 	}
 
 	public static void main(String[] args) {
-		System.out.println("this is master demo 01 04");
+		System.out.println("this is master demo 01 05");
 	}
 
 	public void masteradd(){
